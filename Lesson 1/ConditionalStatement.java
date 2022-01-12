7aea66fae4b6000ec895e4e1e07770ace8cf0c20
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-12 13:44:24 +0300

    Исправил ошибки

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,32 +1,32 @@
 public class ConditionalStatement {
     
-    public static void main (String[]args) {
-        int age =27;
-        char sex = 'M';
-        double height = 1.80;
-        char firstLetterOfName = 'M';
-        char letterOfName = 'I';
-
+    public static void main(String[] args) {
+        int age = 27;
         if (age > 20) {
             System.out.println("Вы взрослый");
         }
+
+        char sex = 'M';
         if (sex == 'M') {
             System.out.println("Вы мужчина");
         } else if (sex != 'M') {
             System.out.println("Вы женщина");
         }
+
+        double height = 1.80;
         if (height < 1.80) {
             System.out.println("Вам надо подрасти");
         } else {
             System.out.println("У вас нормальный рост");
         }
+
+        char firstLetterOfName = 'M';
         if (firstLetterOfName == 'M') {
             System.out.println("Первая буква имени М");
-        } else if (letterOfName == 'I') {
+        } else if (firstLetterOfName == 'I') {
             System.out.println("Первая буква имени И");
         } else {
             System.out.println("Первая буква имени неизвестна");
         }
-
     }
 }
