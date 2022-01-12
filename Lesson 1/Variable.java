708f89e3fb3a204f1f4c96b1ec99dd1384cf9644
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-12 13:37:07 +0300

    Исправил ошибки

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,9 +1,10 @@
 public class Variable {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         byte core = 4;
         short typeSystem = 64;
         int processor = 7310;
+        long numberOfComputer = 52261;
         float freeMemory = 2.46f;
         double diskBMemory = 155.7;
         char disk = 'C';
@@ -11,6 +12,7 @@ public class Variable {
         System.out.println(core);
         System.out.println(typeSystem);
         System.out.println(processor);
+        System.out.println(numberOfComputer);
         System.out.println(freeMemory);
         System.out.println(diskBMemory);
         System.out.println(disk);
