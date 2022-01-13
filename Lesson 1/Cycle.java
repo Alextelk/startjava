6da857f2bd65701fc1eb3d88d61be871b9ade76c
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-13 16:34:14 +0300

    Исправил ошибки

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,8 +1,7 @@
 public class Cycle {
 
     public static void main(String[] args) {
-
-       for (int i = 0; i<=20; i++) {
+       for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
         
@@ -11,21 +10,19 @@ public class Cycle {
         int number = 6;
         while (number >= -6) {
             System.out.println(number);
-            number = number - 2;
+            number -= 2;
         }
         
         System.out.println(" ");
 
-        int oddNumber = 10;
-        boolean isTrue;
-        int sum = 0;
+        int numbers = 10;
+        int sumOdd = 0;
         do {
-            isTrue = oddNumber % 2 == 1;
-            if (isTrue) {
-                sum = sum + oddNumber;
+            if (numbers % 2 == 1) {
+                sumOdd += numbers;
              }            
-            oddNumber++;
-        } while (oddNumber < 20);
-        System.out.println(sum);
+            numbers++;
+        } while (numbers < 20);
+        System.out.println(sumOdd);
     }    
 }
