abdf26398ef2c28c00aacb9fd0d24cb315f13a23
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-14 15:01:02 +0300

    Unicode

---
+++ Lesson 1/Unicode.java
@@ -0,0 +1,8 @@
+public class Unicode {
+
+    public static void main(String[] args) {
+        for (char unicodeSymbol = 9398; unicodeSymbol <= 10178; unicodeSymbol++) {
+            System.out.println(unicodeSymbol);
+        }
+    }
+}
