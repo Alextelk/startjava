10439f431df9563d919c0cc7c27015de4a33855c
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-14 11:52:55 +0300

    Подправил Calculator

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -3,29 +3,23 @@ public class Calculator {
     public static void main(String[] args) {
         int firstNumber = 5;
         int secondNumber = 2;
-        char command = '+';
-        int result = 0;
-        if (command == '+') {
-            result = firstNumber + secondNumber;
-            System.out.println(result);
-        } else if (command == '-') {
-            result = firstNumber - secondNumber;
-            System.out.println(result);
-        } else if (command == '*') {
-            result = firstNumber * secondNumber;
-            System.out.println(result);
-        } else if (command == '/') {
-            result = firstNumber / secondNumber;
-            System.out.println(result);
-        } else if (command == '^') {
-            result = 1;
+        char mathOperation = '+';
+        int result = 1;
+        if (mathOperation == '+') {
+            System.out.println(firstNumber + secondNumber);
+        } else if (mathOperation == '-') {
+            System.out.println(firstNumber - secondNumber);
+        } else if (mathOperation == '*') {
+            System.out.println(firstNumber * secondNumber);
+        } else if (mathOperation == '/') {
+            System.out.println(firstNumber / secondNumber);
+        } else if (mathOperation == '^') {
             for (int i = 1; i <= secondNumber; i++) {
                 result *= firstNumber;
             }
             System.out.println(result);
-        } else if (command == '%') {
-            result = firstNumber % secondNumber;
-            System.out.println(result);
+        } else if (mathOperation == '%') {
+            System.out.println(firstNumber % secondNumber);
         } else {
             System.out.println("Такой команды нет");
         }
