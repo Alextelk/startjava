bc8400fc715d329456f6cdd4388d612cdfbbeabf
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-20 15:14:00 +0300

    Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    char sex = 'M';
+    String name = "Alexandr";
+    float height = 1.8f;
+    float weight = 70.5f;
+    int age = 27;
+
+    void walk() {
+        System.out.println("Прошел несколько шагов");
+    }
+
+    String sit() {
+        return "sit";
+    }
+
+    void action() {
+        System.out.println("Побежал");
+    }
+
+    String say() {
+        return "say";
+    }
+
+    boolean learnJava() {
+        return true;
+    }
+}
