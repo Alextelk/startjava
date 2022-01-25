2ba78576a5660f7d0282497c3f788d4c896d0f24
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-25 19:43:48 +0300

    Wolf

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,36 @@
+public class Wolf {
+
+    public char sex;
+    public String name;
+    public int weight;
+    public int age;
+    public String color;
+
+    public void walk() {
+        System.out.println("Идет");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
+    }
+}
+
+
+// Создайте класс WolfTest с методом main
+// в нем создайте объект типа Wolf
+// в этом же классе присвойте полям Wolf какие-то значения
+// получите эти значения из полей и отобразите в консоли
+// вызовите методы объекта
+// Покажите Д/З наставнику
