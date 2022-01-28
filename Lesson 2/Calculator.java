517a3e1b2c8d9806e4b98be8cb70a303d9648e3a
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-01-28 16:00:40 +0300

    Calculator

---
+++ Lesson 2/Calculator.java
@@ -0,0 +1,60 @@
+public class Calculator{
+
+    private int firstNumber;
+    private char mathOperation;
+    private int secondNumber;
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public char getMathOperation() {
+        return mathOperation;
+    }
+
+    public void setMathOperation(char mathOperation) {
+        this.mathOperation = mathOperation;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public void calculation (int firstNumber, char mathOperation, int secondNumber) {
+        switch(mathOperation) {
+        case '+':
+            System.out.println(firstNumber + secondNumber);
+            break;
+        case '-':
+            System.out.println(firstNumber - secondNumber);
+            break;
+        case '*':
+            System.out.println(firstNumber * secondNumber);
+            break;
+        case '/':
+            System.out.println(firstNumber / secondNumber);
+            break;
+        case '%':
+            System.out.println(firstNumber % secondNumber);
+        case '^':
+            int result = 1;
+            for(int i = 1; i <= secondNumber; i++) {
+                result *= firstNumber;
+            }
+            System.out.println(result);
+            break;
+        default:
+            System.out.println("Такой операции нет");
+            break;
+        }
+    }
+}
+
