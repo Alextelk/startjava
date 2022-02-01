426481223ee3128fdc039dd83191f81715cb37f3
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-02-01 14:53:06 +0300

    JaegerTest

--- Lesson 2/JaegerTest.java
+++ Lesson 2/JaegerTest.java
@@ -1,11 +1,7 @@
 public class JaegerTest {
 
     public static void main(String[] args) {
-        Jaeger typhoon = new Jaeger();
-        typhoon.setModelName("Crimson Typhoon");
-        typhoon.setMark("Mark-4");
-        typhoon.setStrength(8);
-        typhoon.setArmor(6);
+        Jaeger typhoon = new Jaeger("Crimson Typhoon", "Mark-4", 8, 6);
         System.out.println(typhoon.getModelName());
         System.out.println(typhoon.getMark());
         System.out.println(typhoon.getStrength());
@@ -15,11 +11,7 @@ public class JaegerTest {
         typhoon.useWeapon();
         System.out.println(typhoon.scanKaiju() + "\n");
 
-        Jaeger danger = new Jaeger();
-        danger.setModelName("Gipsy Danger");
-        danger.setMark("Mark-3");
-        danger.setStrength(8);
-        danger.setArmor(6);
+        Jaeger danger = new Jaeger("Gipsy Danger", "Mark-3", 8, 6);
         System.out.println(danger.getModelName());
         System.out.println(danger.getMark());
         System.out.println(danger.getStrength());
