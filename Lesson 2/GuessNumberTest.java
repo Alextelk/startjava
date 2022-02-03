b3f73163bfe8705e9a69d4f7dda1174bf587b527
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-02-03 14:53:03 +0300

    GuessNumberTest

---
+++ Lesson 2/GuessNumberTest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in, "cp866");
+        System.out.println("Игрок-1 введите имя");
+        String firstPlayerName = scan.nextLine();
+        Player firstPlayer = new Player(firstPlayerName);
+        System.out.println("Игрок-2 введите имя");
+        String secondPlayerName = scan.nextLine();
+        Player secondPlayer = new Player(secondPlayerName);
+        GuessNumber game = new GuessNumber(firstPlayer, secondPlayer);
+
+        String option;
+        do {
+            game.guessNumber();
+            System.out.println("Хотите повторить? yes/no");
+            option = scan.nextLine();
+            if(option.equals("yes")) {
+                game.guessNumber();
+            } else {
+                while(!option.equals("no") && !option.equals("yes")) {
+                System.out.println("Введите yes/no");
+                option = scan.nextLine();
+                }
+            }
+        } while(!option.equals("no"));
+    }
+}
