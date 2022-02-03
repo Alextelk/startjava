91e9bb93aad3a51ff23cd0d828b12c9e13b8a447
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-02-03 18:14:57 +0300

    Player

--- Lesson 2/Player.java
+++ Lesson 2/Player.java
@@ -1,7 +1,6 @@
 public class Player {
 
     private String name;
-    private int number;
 
     public Player(String name) {
         this.name = name;
@@ -9,17 +8,5 @@ public class Player {
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number){
-        this.number = number;
     }
 }
