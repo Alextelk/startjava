87ad0db992f851fceebc83eb81aaf6ea056c6471
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-02-25 14:34:47 +0300

    stsrtjava

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Jaegers(
+id          SERIAL PRIMARY KEY, 
+modelName   TEXT, 
+mark        INTEGER, 
+height      REAL, 
+weight      REAL, 
+status      TEXT, 
+origin      TEXT, 
+launch      DATE, 
+kaijuKill   INTEGER);
---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,12 @@
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+    VALUES
+	('Gipsy Danger',     '3',    '79.25',    '1.98',     'Destroyed',    'USA',          '2017-07-10',      '9'),
+        ('Cherno Alpha',     '1',    '85.34',    '2.412',    'Destroyed',    'Russia',       '2015-10-01',      '6'),
+        ('Striker Eureka',   '5',    '76.2',     '1.85' ,    'Destroyed',    'Australia',    '2019-11-02',      '11'),
+        ('Coyote Tango',     '1',    '85.34',    '2.312',    'Destroyed',    'Japan',        '2015-12-30',      '2'),
+        ('Crimson Typhoon',  '4',    '76.2',     '1.722',    'Destroyed',    'China',        '2018-08-22',      '7'),
+        ('Bracer Phoenix',   '5',    '70.91',    '2.128',    'Destroyed',    'USA',          '2025-11-01',      '8'),
+        ('Gipsy Avenger',    '6',    '81.77',    '2.004',    'Destroyed',    'Russia',       '2012-09-11',      '1'),
+        ('Guardian Bravo',   '6',    '73.21',    '1.975',    'Active',       'UK',           '2033-12-15',      '0'),
+        ('Horizon Brave' ,   '1',    '72.54',    '7.89',     'Active',       'China',        '2015-12-22',      '2'),
+        ('Romeo Blue',       '1',    '77.72',    '7.775',    'Active',       'USA',          '2015-12-15',      '2');
