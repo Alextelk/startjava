903d88bf9b408b115240bae9abb34dc9245dad85
Alexandr Telkov <alexandrtelkov94@gmail.com>
2022-02-26 19:37:43 +0300

    startjava

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Jaegers(
-id          SERIAL PRIMARY KEY, 
-modelName   TEXT, 
-mark        INTEGER, 
-height      REAL, 
-weight      REAL, 
-status      TEXT, 
-origin      TEXT, 
-launch      DATE, 
-kaijuKill   INTEGER);
+	id          SERIAL PRIMARY KEY, 
+	modelName   TEXT, 
+	mark        INTEGER, 
+	height      REAL, 
+	weight      REAL, 
+	status      TEXT, 
+	origin      TEXT, 
+	launch      DATE, 
+	kaijuKill   INTEGER
+	);
